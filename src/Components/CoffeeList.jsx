5dfbdcccf96427780cad4f53bff70afd6b41f23d
Jacob Maier <jacobamaier@gmail.com>
2023-04-21 11:23:32 -0700

    update CoffeeList to map inventory.

---
+++ src/Components/CoffeeList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Coffee from "./Coffee";
+
+const CoffeeList = (props) => {
+  return (
+    <>
+      <hr />
+      {props.coffeeList.map((coffee) => (
+        <div>
+          <Coffee
+            whenCoffeeClicked={props.onCoffeeSelection}
+            name={coffee.name}
+            price={coffee.price}
+            quantity={coffee.quantity}
+            id={coffee.id}
+            key={coffee.id}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+CoffeeList.propTypes = {
+  coffeeList: PropTypes.array,
+  onCoffeeSelection: PropTypes.func,
+};
+
+export default CoffeeList;
