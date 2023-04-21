f5589358839dee33a45dc7c635bd07606c71b0bf
Jacob Maier <jacobamaier@gmail.com>
2023-04-21 08:25:28 -0700

    Add initial components.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <InventoryControl />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
